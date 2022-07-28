7f7fcaf2a540d1e652f75898bcfd9d581cf639fb
Skunko-Hilarious <75173418+Skunko-Hilarious@users.noreply.github.com>
2022-07-28 10:20:56 +0200

    Added +10 and -10 and reset buttons to Javatest tally counter

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+//initialization
 let num1 = 0;
 let thisP = document.getElementById("aParagraph");
 let changeP = thisP.textContent;
 thisP.innerHTML = changeP+" "+num1;
 
+//Addition Button
 document.getElementById("addButton").onclick = function(){
-    console.log(thisP);
     num1 +=1;
     thisP.innerHTML = changeP+" "+num1;
 }
 
+//Subtraction Button
 document.getElementById("subtractButton").onclick = function(){
-    console.log(thisP);
     if (num1 > 0){
         num1 -=1;
+    }
+    thisP.innerHTML = changeP+" "+num1;
+}
+
+//Reset/set 0 button
+document.getElementById("resetButton").onclick = function(){
+    num1 = 0;
+    thisP.innerHTML = changeP+" "+num1;
+}
+
+//Add 10
+document.getElementById("plusTen").onclick = function(){
+    num1 += 10;
+    thisP.innerHTML = changeP+" "+num1;
+}
+
+//Subtract 10
+document.getElementById("minusTen").onclick = function(){
+    if (num1 > 9){
+        num1 -= 10;
     }
     thisP.innerHTML = changeP+" "+num1;
 }
