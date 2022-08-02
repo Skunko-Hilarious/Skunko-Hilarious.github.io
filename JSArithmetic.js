26b7d0942c68878ff1be06f9e2d91af578c33afd
Skunko-Hilarious <75173418+Skunko-Hilarious@users.noreply.github.com>
2022-08-02 10:04:12 +0200

    Added JSArithmetic Challenge

---
+++ JSArithmetic.js
@@ -0,0 +1,41 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+   let circRadius = 25;
+   let rectWidth = 10;
+   let rectHeight = 17;
+   let triWidth = 20;
+   let triHeight = 9;
+
+
+// Rectangle area calculation
+   let rectArea = rectWidth * rectHeight;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : "+ rectArea);
+
+
+console.log("\n");
+
+
+// circle area calculation
+   let circArea = pi*circRadius**2;
+
+// Append your circle answer:
+console.log("The area of the given circle is : "+circArea);
+
+
+console.log("\n");
+
+
+// triangle area calculation
+   let triArea = triWidth * triHeight * half;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : "+triArea );
